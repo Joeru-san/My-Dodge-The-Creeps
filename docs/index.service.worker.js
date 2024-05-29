5bb2dc64e3f14e92a789b61d738af0ffbd6052bd
Joeru-san <gioele.vicario480@gmail.com>
2024-05-29 09:50:57 +0200

    New Release for Github Pages

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1716797641|7582448115";
+const CACHE_VERSION = "1716969007|3748157960";
 const CACHE_PREFIX = "Dodge The Creeps-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
